5db83977aca1926c63e7cf6cbd6fb9530210d9b4
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-04-30 18:18:52 +0600

    server loading fixing

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const cors = require("cors");
 const express = require("express");
-const port = process.env.PORT || 3000;
+const cors = require("cors");
 const app = express();
-app.use(cors());
-app.use(express.json());
+const port = process.env.port || 3000;
 require("dotenv").config();
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://toy-market-84c26.web.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "authorization"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Toy Market Server!");
 });
 
+const { MongoClient, ServerApiVersion, Db, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASWORD}@cluster0.1ranzbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASWORD}@cluster0.1ranzbu.mongodb.net/?retryWrites=true&w=majority`;
-
+// const uri = "mongodb://localhost:27017";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,11 +29,10 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+// const client = new MongoClient(uri);
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     const toymarket = client.db("toymarket");
     const allToys = toymarket.collection("allToys");
 
@@ -76,10 +81,10 @@ async function run() {
       res.send(result);
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
   }
